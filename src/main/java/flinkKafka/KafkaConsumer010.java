98c372b8a250bc44d0204c2348a27fb87b77528a
jess639 <lee2pengjess@163.com>
2019-08-26 06:10:20 +0800

    flink消费kafka实例，并且将消费数据推送到kafka另外一个话题中

---
+++ src/main/java/flinkKafka/KafkaConsumer010.java
@@ -0,0 +1,40 @@
+package flinkKafka;
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer010;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+
+import java.util.Properties;
+
+public class KafkaConsumer010 {
+
+	public static void main(String[] args) throws Exception {
+	   final Properties pro = new Properties();
+		pro.put("bootstrap.servers","allen:9092") ;
+        pro.put("group.id","jess10") ;
+        pro.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        pro.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		 
+		DataStream<String> input = env
+				.addSource(
+						new FlinkKafkaConsumer010<String>(
+								"abc",
+								new SimpleStringSchema(),
+								pro) ) ;
+
+
+		input.print();
+
+
+		input.addSink(
+				new FlinkKafkaProducer010<String>( "bcd",
+						new SimpleStringSchema(),
+						pro));
+
+		env.execute("Kafka 0.10 Example");
+	}
+}
